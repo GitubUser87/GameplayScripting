6b142b2b2daeaaa082583700b5448f9a2da8b1cf
GitubUser87 <153625913+GitubUser87@users.noreply.github.com>
2024-03-22 13:53:25 +0000

    Mini Update - 22/03/2024
    This is just to record videos and reset Level1-3 start position. It also adds in a double shot mechanic for Level 2.

--- D3303414 - Project Files 2D URP/Assets/Scripts/Level2/Bullets/Shooting.cs
+++ D3303414 - Project Files 2D URP/Assets/Scripts/Level2/Bullets/Shooting.cs
@@ -5,23 +5,42 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     public Transform bulletSpawnPoint;
+    public Transform doublebulletSpawnPoint;
+    public Transform double2bulletSpawnPoint;
     public GameObject bulletPrefab;
-    public GameObject bulletPrefab2;
+    
     public float bulletSpeed = 10;
     public float fireRate = 0.2f;
     float lastShotTime = 0;
- 
+    public bool doubleShot;
     void Update()
     {
         if (Input.GetButton("Fire1"))
         {
             if (Time.time > lastShotTime + fireRate)
             {
-                //This will spawn in a new bullet everytime the command is initiated.
-                var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-                bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
-                lastShotTime = Time.time;
-                FindObjectOfType<AudioManager>().Play("PlayerShoot");
+                if (doubleShot == false)
+                {
+
+                    //This will spawn in a new bullet everytime the command is initiated.
+                    var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                    bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+                    lastShotTime = Time.time;
+                    FindObjectOfType<AudioManager>().Play("PlayerShoot");
+                }
+
+                else if (doubleShot == true)
+                {
+                    var bullet = Instantiate(bulletPrefab, doublebulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                    bullet.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+                    lastShotTime = Time.time;
+
+                    var bullet2 = Instantiate(bulletPrefab, double2bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                    bullet2.GetComponent<Rigidbody2D>().velocity = bulletSpawnPoint.up * bulletSpeed;
+                    lastShotTime = Time.time;
+                    FindObjectOfType<AudioManager>().Play("PlayerShoot");
+                }
+
             }
         }
     }

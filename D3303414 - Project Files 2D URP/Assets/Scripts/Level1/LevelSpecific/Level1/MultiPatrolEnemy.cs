2539bcd1aebda52dfc88cb0f37e1f49b2418b13d
GitubUser87 <153625913+GitubUser87@users.noreply.github.com>
2024-04-22 12:04:18 +0100

    Small Update - 22/04/2024
    This Update consists of adding a new area to the final level of game 1, and beginning to add new enemies.

---
+++ D3303414 - Project Files 2D URP/Assets/Scripts/Level1/LevelSpecific/Level1/MultiPatrolEnemy.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiPatrolEnemy : MonoBehaviour
+{
+    public float speed = 1f;
+    public Vector2[] waypoints;
+
+    Rigidbody2D body;
+    int wayPointIndex = 0;
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        body = GetComponent<Rigidbody2D>();
+        transform.position = waypoints[0]; //This sets the entered waypoints as the new positions.
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 newPosition = Vector2.MoveTowards(body.position, waypoints[wayPointIndex], speed); //This will set the new waypoint as the objects new destination.
+        body.MovePosition(newPosition);
+        if (Vector2.Distance(newPosition, waypoints[wayPointIndex]) < 0.1f)
+        {
+            wayPointIndex = (wayPointIndex + 1) % waypoints.Length;
+            spriteRenderer.flipX = true;
+        }
+        else if (wayPointIndex == 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+    }
+    void OnDrawGizmosSelected() //This will present the waypoints connected by a line in editor mode.
+    {
+        Gizmos.color = Color.yellow;
+        Vector2 lastPosition = waypoints[0];
+        foreach (Vector2 waypoint in waypoints)
+        {
+            Gizmos.DrawLine(lastPosition, waypoint);
+            Gizmos.DrawSphere(waypoint, 0.3f);
+            lastPosition = waypoint;
+        }
+    }
+}
